2886e58e66f30d687f65b62d81b9ea6c97d79703
Ambreen Riaz <159724062+Ambreenriaz13@users.noreply.github.com>
2024-03-23 23:10:11 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+var GuestList = ["Saba", "Sana", "Laila", "Ayesha", "Fatima"];
+//GuestList.forEach(oneGuest => console.log(`Salam${oneGuest}, would you like to dinner with me`));
+var lengthguest = GuestList.length;
+console.log("we are inviting total ".concat(lengthguest, " geust."));
---
+++ main.ts
@@ -0,0 +1,7 @@
+let GuestList = ["Saba" ,"Sana" ,"Laila" ,"Ayesha","Fatima" ];
+
+//GuestList.forEach(oneGuest => console.log(`Salam${oneGuest}, would you like to dinner with me`));
+
+let lengthguest: number= GuestList.length;
+
+console.log(`we are inviting total ${lengthguest} geust.`);
